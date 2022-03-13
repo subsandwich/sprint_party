4377cdb0e6cc7884bfc93656c8e3e3f4d642af85
jake612 <jake612@live.unc.edu>
2022-03-13 19:46:03 -0400

    Properly listening to correct button

--- background.js
+++
@@ -1,9 +0,0 @@
-// Check on refreshes for buttonv
-// SO reference: https://stackoverflow.com/questions/20865581/chrome-extension-content-script-not-loaded-until-page-is-refreshed
-console.log("Background!");
-chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-  console.log("hello world");
-  console.log("TabId: " + tabId)
-  console.log("Change infor" + changeInfo.url);
-  console.log("Tab url" + tab.url);
-});
--- sprint-party.js
+++ sprint-party.js
@@ -1,16 +1,33 @@
 
-console.log("I'm here");
 
+let startButton = null;
+
+
+// Callback function to execute when mutations are observed
+const portalCallback = function(mutationsList, observer) {
+    // Use traditional 'for loops' for IE 11
+    for(const mutation of mutationsList) {
+        if (mutation.type === 'childList' && mutation.removedNodes.length > 0 && mutation.removedNodes[0].className == "atlaskit-portal") {
+            startButton = null;
+            console.log("start button removed!");
+        }
+        else if (mutation.type === 'attributes' && startButton == null) {
+                startButton = Array.from(document.getElementsByClassName("css-19r5em7")).filter(e=> e.innerHTML=="Start")[0];
+                if (startButton) {
+                   startButton.addEventListener("mouseover", (e)=>{console.log("Let's get this party started")});
+                }
+
+
+        }
+    }
+};
 
 window.addEventListener('load', (event) => {
-    console.log("I'm there");
-    var sprintButton = Array.from(document.getElementsByClassName("css-19r5em7")).filter(e=> e.innerHTML=="Start sprint")[0];
-    console.log(sprintButton);
-    sprintButton.addEventListener("mouseover", handleSprintButtonClick);
+    console.log("Ready to party!");
+    // Begin listening
+    var portalContainer = document.getElementsByClassName("atlaskit-portal-container")[0];
+    // Options for the observer (which mutations to observe)
+    const config = { attributes: true, childList: true, subtree: true };
+    const observer = new MutationObserver(portalCallback);
+    observer.observe(portalContainer, config);
 });
-
-
-function handleSprintButtonClick(){
-    console.log("Hello World");
-}
-
